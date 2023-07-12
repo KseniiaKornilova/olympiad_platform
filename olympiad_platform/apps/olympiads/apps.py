dec149aeb1565b1fc1304892bdc4b83b40b930fd
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-07-12 14:32:02 +0600

    feat: create olympiads app

---
+++ olympiad_platform/apps/olympiads/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OlympiadsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.olympiads'
+    verbose_name = 'Олимпиады'
