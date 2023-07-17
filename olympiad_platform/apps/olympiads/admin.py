09a0f4263488c8997641dba0ed03d21af06cc389
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-07-17 17:08:03 +0600

    feat(admin.py) set administrative panel

--- olympiad_platform/apps/olympiads/admin.py
+++ olympiad_platform/apps/olympiads/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from .models import Olympiad, Subject, OlympiadUser, QuestionSection, Question, UserAnswer
 
 # Register your models here.
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    fields = ('name',)
+    list_display_links = ('name',)
+
+class OlympiadUserInline(admin.TabularInline):
+    model = OlympiadUser
+    extra = 1
+
+
+class OlympiadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subject', 'degree')
+    list_display_links = ('title',)
+    search_fields = ('^title', '^subject')
+    list_per_page = 10
+    list_filter = ('subject',)
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('title', 'subject', 'degree'),
+            'classes': ('wide',),
+        }),
+        ('Временные данные', {
+            'fields': ('date_of_start', 'registration_dedline', 'olympiad_duration'),
+            'classes': ('wide',),
+        }),
+    )
+    inlines = (OlympiadUserInline,)
+    autocomplete_fields = ('participants',)
+
+
+class QuestionSectionAdmin(admin.ModelAdmin):
+    list_display = ('section', 'points')
+    list_display_links = ('section',)
+    search_fields = 'section',
+    fields = ('section', 'points')
+    
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_description', 'section', 'olympiad', 'correct_answer')
+    list_display_links = ('question_description',)
+    search_fields = ('question_description',)
+    list_filter = ('olympiad',)
+    fields = ('question_description', 'section', 'olympiad', 'correct_answer')
+
+
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'answer', 'score')
+    list_display_links = ('user',)
+    search_fields = ('user', '^olympiad')
+    
+    
+    
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Olympiad, OlympiadAdmin)
+admin.site.register(QuestionSection, QuestionSectionAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(UserAnswer, UserAnswerAdmin)
--- olympiad_platform/apps/students/admin.py
+++ olympiad_platform/apps/students/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import User
+from ..olympiads.models import OlympiadUser
 
 # Register your models here.
+class OlympiadInline(admin.TabularInline):
+    model = OlympiadUser
+    exclude = ('answers', 'score')
+    extra = 1
+    
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'email', 'status', 'degree', 'degree_id', 'is_active')
+    list_display_links = ('__str__',)
+    search_fields = ('^__str__', 'email')
+    list_filter = ('status',)
+    list_per_page = 15
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('last_name', 'first_name', 'patronymic', 'email', 'birthday'),
+            'classes': ('wide',),
+        }),
+        ('Данные о школе', {
+            'fields': ('status', ('degree', 'degree_id')),
+            'classes': ('wide',),
+        }),
+    )
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj=None)
+        if obj:
+            if obj.is_staff:
+                fieldsets = (
+                ('Основная информация', {
+                'fields': ('last_name', 'first_name', 'patronymic', 'email', 'birthday'),
+                'classes': ('wide',),
+            }),
+                )
+        return fieldsets
+
+    inlines = (OlympiadInline,)
+    def get_inlines(self, request, obj=None):
+        inlines = super().get_inlines(request, obj=None)
+        if obj:
+            if obj.is_staff:
+                inlines = ()
+        return inlines
+    
+admin.site.register(User, UserAdmin)
