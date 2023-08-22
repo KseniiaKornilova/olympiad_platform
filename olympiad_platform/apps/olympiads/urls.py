c12ac658dc4ab3f5a043774057c6c34fc715e358
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-08-22 17:35:32 +0600

    refactor: add app's name

--- olympiad_platform/apps/olympiads/urls.py
+++ olympiad_platform/apps/olympiads/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'olympiads'
+
 urlpatterns = [
     path('', views.main_page, name='main_page'),
 ]
--- olympiad_platform/apps/olympiads/views.py
+++ olympiad_platform/apps/olympiads/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return render(request, 'index.html')
+    return render(request, 'olympiads/index.html')
