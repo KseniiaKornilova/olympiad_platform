9c895634b7b8239e202a7622c0f842eca086b174
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-08-23 16:09:00 +0600

    feat: add logout system and profile page

--- olympiad_platform/apps/students/urls.py
+++ olympiad_platform/apps/students/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import UserLogin
+from .views import UserLogin, profile, UserLogout
 
 app_name = 'students'
 
 urlpatterns = [
     path('accounts/login/', UserLogin.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'), 
+    path('accounts/logout/', UserLogout.as_view(), name='logout'),
 ]
--- olympiad_platform/apps/students/views.py
+++ olympiad_platform/apps/students/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
@@ -9,5 +11,10 @@ from .forms import UserForm
 class UserLogin(LoginView):
     template_name = 'students/login.html'
     
-    
-    
+@login_required  
+def profile(request):
+    return render(request, 'students/profile.html')
+
+
+class UserLogout(LogoutView, LoginRequiredMixin):
+    template_name = 'students/logout.html'
