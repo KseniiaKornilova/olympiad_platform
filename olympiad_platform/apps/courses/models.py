6c4e6242e1c66e19b1e87964d0e19fa4c16b08f3
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-09-07 13:42:22 +0600

    feat: create course's models

--- olympiad_platform/apps/courses/models.py
+++ olympiad_platform/apps/courses/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from ..olympiads.models import Subject
+from ..students.models import User
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, unique=True, verbose_name='Название курса')
+    course_description = models.TextField(verbose_name='Описание курса', blank=True, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='Название дисциплины')
+    month_amount = models.SmallIntegerField(verbose_name='Продолжительность', blank=True, null=True)
+    times_a_week = models.SmallIntegerField(verbose_name='Раз в неделю', blank=True, null=True)
+    participants = models.ManyToManyField(User, through='CourseUser', verbose_name='Ученики курса')
+    image = models.CharField(verbose_name='Путь до изображения от static директории', null=True, blank=True)
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['subject', 'title']
+
+
+class CourseUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Имя ученика')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+    class Meta: 
+        verbose_name = 'Регистрация ученика на курс'
+        verbose_name_plural = 'Регистрации учеников на курсы'
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, unique=True, verbose_name='Название урока')
+    number = models.SmallIntegerField(verbose_name='Номер урока') 
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Название курса')
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta: 
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
