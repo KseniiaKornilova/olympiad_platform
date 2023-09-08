68d69af53e13121de670ef7f79e56a2b0d138793
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-09-08 18:16:02 +0600

    feat: add courses' list and search ability

--- olympiad_platform/apps/courses/urls.py
+++ olympiad_platform/apps/courses/urls.py
@@ -5,4 +5,5 @@ app_name = 'courses'
 
 urlpatterns = [
     path('courses/list/', views.UserCoursesList.as_view(), name='courses_list'),
+    path('courses/', views.CourseList.as_view(), name='courses'),
 ]
--- olympiad_platform/apps/courses/views.py
+++ olympiad_platform/apps/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Course
+from ..olympiads.models import Subject
 from ..olympiads.forms import SearchForm
 
 # Create your views here.
@@ -19,6 +20,31 @@ class UserCoursesList(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['form'] = SearchForm(self.request.GET)
+        return context
+
+
+class CourseList(ListView):
+    template_name = 'courses/all_courses.html'
+    model = Course
+    context_object_name = 'courses'
+
+    def get_queryset(self):
+        category = self.request.GET.get('category', None)
+        school_subject = self.request.GET.get('subject', None)
+        search_word = self.request.GET.get('keyword')
+        queryset = Course.objects.all()
+        if category:
+            queryset = queryset.filter(category__exact=category)
+        if school_subject:
+            queryset = queryset.filter(subject__exact=school_subject)
+        if search_word:
+            queryset = queryset.filter(title__icontains=search_word)
+        return queryset
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subjects'] = Subject.objects.all()
         context['form'] = SearchForm(self.request.GET)
         return context
 
