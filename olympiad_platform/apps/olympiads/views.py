44a8f56d40a60390ddf704359790b209b6d5e9c4
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-10-18 16:04:19 +0600

    style: add proper names for olympiad's reference

--- olympiad_platform/apps/olympiads/views.py
+++ olympiad_platform/apps/olympiads/views.py
@@ -34,6 +34,9 @@ class UserOlympiadList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = SearchForm(self.request.GET)
+        student = self.request.user
+        user_submissions = OlympiadUser.objects.filter(user=student)
+        context['user_submissions'] = user_submissions
         return context
 
 
