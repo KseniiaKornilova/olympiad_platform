4c92cc9b8496d1a6b5b44a3b51815403aee0162d
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2024-08-09 17:28:39 +0500

    feat: add django debug toolbar

--- olympiad_platform/olympiad/settings.py
+++ olympiad_platform/olympiad/settings.py
@@ -165,3 +165,5 @@ CACHES = {'default': {
     'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
     'LOCATION': '127.0.0.1:11211',
                 }}
+
+INTERNAL_IPS = ['127.0.0.1',]
