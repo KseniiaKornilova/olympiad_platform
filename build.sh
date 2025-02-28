fc8fb72038d9c15bc993e533f1944478c386a4a4
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2025-02-28 18:15:00 +0500

    bug: change path to build.sh and content

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit  
 
-poetry install  
+cd /opt/render/project/src
+poetry install --only main
 python manage.py migrate  
 python manage.py collectstatic --no-input 
